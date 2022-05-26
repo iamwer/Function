6a471a00686f235ba4126a5adfee54f5fa2ad122
alexandr shishigin <iamwer4@gmail.com>
2022-05-26 22:01:15 +0900

    Добавьте файлы проекта.

---
+++ Function/Function.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+
+
+
+
+int main()
+{
+	int number;
+	cout << "Number: ";
+	cin >> number;
+
+
+	bool isPrimeFlag = true;
+
+	for (int i = 2; i < number; i++)
+	{
+		if (number % i == 0)
+		{
+			isPrimeFlag = false;
+			break;
+		}
+	}
+
+	if (isPrimeFlag)
+		cout << "Prime number" << endl;
+	else 
+		cout << "Not prime number" << endl;
+
+
+
+	system("pause>0");
+}
